8c58f428f9f5482ea5fb5092e6a3728ea48175f0
SajidI17 <47987515+SajidI17@users.noreply.github.com>
2019-04-08 11:38:52 -0400

    Update objects.js

--- www/js/objects.js
+++ www/js/objects.js
@@ -40,7 +40,7 @@ class Tile extends Sprite{ //core maze graphical and functional component
             case 2: imgPath = "www/img/tilesets/portal.png"; //portals
                     _collision = false;
                     break;
-            case 3: imgPath = "www/img/tilesets/heart.png"; //coin
+            case 3: imgPath = "www/img/tilesets/Heart.png"; //coin
                     _collision = false;
                     break;
             case 4: imgPath = "www/img/tilesets/end.png"; //end
